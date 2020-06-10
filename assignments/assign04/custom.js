a08f40023091e594e65851a46393440d55c9de93
varshakumari97 <varshibather@gmail.com>
2020-06-10 10:52:56 +0500

    my portfolio

---
+++ assignments/assign04/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay: true,
+        autoplayTimeout:3000,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:4,
+            }
+        }
+    })
+  });
